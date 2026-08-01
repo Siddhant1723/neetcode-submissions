9913845fa7d5b042472ee298028857f18c02fc0b
Xt-60 <siddhantp386165@gmail.com>
2026-08-01 19:15:12 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,32 @@
+class MinStack {
+int min=0;
+private Deque<Integer> stack;
+    public MinStack() {
+        stack=new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+       stack.push(val);
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        min=stack.peek();
+        Deque<Integer> tmp=new ArrayDeque<>();
+        while(!stack.isEmpty()){
+            min=Math.min(min,stack.peek());
+            tmp.push(stack.pop());
+        }
+        while(!tmp.isEmpty()){
+            stack.push(tmp.pop());
+        }
+        return min;
+    }
+}
